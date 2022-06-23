70001b7b89db3de2c634213050331d0fd6c6ef4b
Reed Graf <78926953+ReedGraf@users.noreply.github.com>
2022-06-23 17:47:19 -0500

    New looks!
    Needed to make my site look better than it did so I decided to spend some of my time improving the looks of it. There will be more pages coming soon with time.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-window.onload = function(){
-  setTimeout(function(){
-    document.getElementById("loader").remove();
-  },1000);
-};
+//                 _       _     _     
+//                (_)     | |   (_)    
+//   ___  ___ _ __ _ _ __ | |_   _ ___ 
+//  / __|/ __| '__| | '_ \| __| | / __|
+//  \__ \ (__| |  | | |_) | |_ _| \__ \
+//  |___/\___|_|  |_| .__/ \__(_) |___/
+//                  | |        _/ |    
+//                  |_|       |__/     
+
+var numberVar = 0;
+function doNothing() {
+  numberVar += 1;
+  console.log("ok x" + numberVar);
+}
+// It does nothing but count up, I just wanted to put something here.
